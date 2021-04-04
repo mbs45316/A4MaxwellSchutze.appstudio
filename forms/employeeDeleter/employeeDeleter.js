81cc0fbcf3083e5963619ef93122177f652f2149
Maxwell Schutze <mbs45316@creighton.edu>
2021-04-04 18:03:00 -0500

    Initial commit

---
+++ forms/employeeDeleter/employeeDeleter.js
@@ -0,0 +1,41 @@
+
+
+rbtnDelete.onchange = function() {
+    if (typeof(s) == "object") {
+        return
+    } else {
+        let userChoice = $("input[name=rbtnDelete]:checked").prop("value")
+
+        query = `DELETE FROM customer WHERE name = '${userChoice}'`
+        console.log(query)
+        req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+        if (req.status == 200) //transit worked.
+            if (req.responseText == 500)
+                lblMessage2.textContent = `You have successfully deleted the employee named ${userChoice}`
+            else
+                lblMessage2.textContent = `There was a problem deleting ${userChoice} from the database.`
+        else
+            lblMessage2.textContent = `Error: ${req.status}`
+    }
+}
+
+btnDisplay.onclick = function() {
+    rbtnDelete.clear()
+    lblMessage2.value = ''
+    rbtnDelete.items = ''
+    query = "SELECT name FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        if (results.length == 0)
+            lblMessage2.value = "There are no employees in the database."
+        else {
+            for (i = 0; i < results.length; i++)
+                rbtnDelete.addItem(results[i])
+        }
+
+    } else // the transit didn't work - bad wifi? server turned off?
+        lblMessage2.value = "Error code: " + req.status
+}
---
+++ forms/statePicker/statePicker.js
@@ -0,0 +1,41 @@
+netID = 'mbs45316'
+pw = 'UCTRMX'
+
+drpEmployee.onshow = function() {
+    drpEmployee.clear()
+    query = "SELECT state FROM customer"
+    req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+    if (req.status == 200) { //transit trip worked. 
+        results = JSON.parse(req.responseText)
+        if (results.length == 0)
+            lblMessage1.value = "There are no employees from this state in the database."
+        else {
+            for (i = 0; i < results.length; i++)
+                drpEmployee.addItem(results[i])
+        }
+    } else // the transit didn't work - bad wifi? server turned off?
+        lblMessage1.value = "Error code: " + req.status
+}
+
+drpEmployee.onclick = function(s) {
+    if (typeof(s) == "object") {
+        return
+    } else {
+        drpEmployee.value = s // make dropdown show choice the user made
+        query = `SELECT name FROM customer WHERE state = '${s}'`
+        console.log(query)
+        req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
+
+        if (req.status == 200) { //transit trip worked. 
+            results = JSON.parse(req.responseText)
+            if (results.length == 0)
+                lblMessage1.value = "There are no employees from this state in the database."
+            else {
+                for (i = 0; i < results.length; i++)
+                    lstEmployee.addItem(results[i])
+            }
+        } else // the transit didn't work - bad wifi? server turned off?
+            lblMessage1.value = "Error code: " + req.status
+    }
+}
